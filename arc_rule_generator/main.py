8d9b9a9b0eee865f8c87a2d4a6cd03522f2acc22
Yavuz Karaca <yavkaraca@gmail.com>
2026-09-11 22:05:06 +0200

    recolor rules updated

--- arc_rule_generator/main.py
+++ arc_rule_generator/main.py
@@ -40,29 +40,7 @@ from rules.recolor import (
 
 
 RULES = {
-    "occlusion.occlusion_reversal": generate_occlusion_reversal,
-    "occlusion.mirror_x": generate_occlusion_mirror_x,
-    "occlusion.mirror_y": generate_occlusion_mirror_y,
-    "occlusion.rotate_90": generate_occlusion_rotate_90,
-    "occlusion.rotate_180": generate_occlusion_rotate_180,
 
-    "attraction.color_attraction": generate_color_attraction,
-    "attraction.size_attraction": generate_size_attraction,
-    "attraction.falling_blocks": generate_falling_blocks,
-    "attraction.floating_blocks": generate_floating_blocks,
-    "attraction.color_repulsion": generate_color_repulsion,
-
-    "expansion.star_step": generate_star_expansion_single_step,
-    "expansion.star_ray": generate_star_expansion_ray,
-    "expansion.plus_step": generate_plus_expansion_single_step,
-    "expansion.plus_ray": generate_plus_expansion_ray,
-    "expansion.3_arm_star_ray": generate_3arm_star_expansion_ray,
-
-    "arithmetic.minority_takeover": generate_minority_takeover,
-    "arithmetic.majority_takeover": generate_majority_takeover,
-    "arithmetic.equalize_colors": generate_equalize_colors,
-    "arithmetic.increment_majority_color": generate_increment_majority_color,
-    "arithmetic.increment_minority_color": generate_increment_minority_color,
 
     "recolor.shape_color_mapping": generate_shape_color_mapping,
     "recolor.touching_edges_recolor": generate_touching_edges_recolor,
